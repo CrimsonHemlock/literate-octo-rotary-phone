71bad541de28696182c101aff554641bfab32b91
Wayan A. Fontaine-Seiler <50877471+CrimsonHemlock@users.noreply.github.com>
2024-07-03 19:59:11 +0000

    Update build.sh - Revert latex engine install
    Revert latex engine install due to build failure

--- build.sh
+++ build.sh
@@ -20,7 +20,6 @@ rpm-ostree install dconf-editor gnome-tweaks guake gnome-console
 rpm-ostree install gnome-shell-extension-appindicator gnome-shell-extension-blur-my-shell gnome-shell-extension-caffeine gnome-shell-extension-dash-to-dock
 rpm-ostree install zsh zsh-autosuggestions zsh-syntax-highlighting
 rpm-ostree install https://launchpad.net/veracrypt/trunk/1.26.7/+download/veracrypt-1.26.7-CentOS-8-x86_64.rpm
-rpm-ostree install texmaker texlive-scheme-full biber
 
 # Package removal
 rpm-ostree uninstall gnome-tour
